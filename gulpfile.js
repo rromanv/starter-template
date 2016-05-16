8689b8d9bc95aa674075f8db9a6118dc89738576
rromanv <rromanv@gmail.com>
2016-05-16 18:59:53 -0500

    added browser-sync... looks like we can work with this

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-var gulp       = require('gulp');
-var pug        = require('gulp-pug');
-var stylus     = require('gulp-stylus');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
+var gulp        = require('gulp');
+var pug         = require('gulp-pug');
+var stylus      = require('gulp-stylus');
+var sourcemaps  = require('gulp-sourcemaps');
+var concat      = require('gulp-concat');
+var browserSync = require('browser-sync').create();
 
 
  
@@ -14,13 +15,28 @@ gulp.task('pug', function() {
   .pipe(gulp.dest('./dist'));
 });
 
+
 gulp.task('stylus', function () {
   return gulp.src('./styl/[^_]*.styl')
     .pipe(sourcemaps.init())
     .pipe(stylus())
     .pipe(concat('style.css'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browserSync.stream());
+});
+
+
+gulp.task('watch', ['pug', 'stylus'], function(gulpCallback) {
+  browserSync.init({
+    server: './dist/',
+    open: true
+  }, function callback() {
+    gulp.watch('./dist/*.html', browserSync.reload);
+    gulp.watch('./styl/*.styl', ['stylus']);
+    gulp.watch('./views/*.pug', ['pug']);
+    gulpCallback();
+  });
 });
 
 
